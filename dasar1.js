bc693dd7db2d06fbbf3a5781e8b15f0b310df548
Aldet <aldetkirana8.com>
2023-09-20 14:43:33 +0700

    first upload

---
+++ dasar1.js
@@ -0,0 +1,55 @@
+//cara membuat variabel
+var a = "a";
+let b = "a";
+const c = "a";
+
+//string(text), integer(number), boolean, array, object
+let nama = "aldet"; //string
+let umur = 20; //integer
+let affahiyyah = true; //boolean
+let jumlah = 1200.2; //decimal
+
+//Operator Aritmatika
+let angka1 = 10;
+let angka2 = 30;
+console.log(angka1 + angka2, "penambahan");
+console.log(angka1 - angka2, "pengurangan");
+console.log(angka1 / angka2, "pembagian");
+console.log(angka1 * angka2, "perkalian");
+console.log(angka1 % angka2, "modulus");
+
+//
+let OperatorAritmatika = angka1 + angka2;
+console.log(OperatorAritmatika);
+
+OperatorAritmatika = angka1 - angka2;
+console.log(OperatorAritmatika);
+
+OperatorAritmatika = angka1 / angka2;
+console.log(OperatorAritmatika);
+
+OperatorAritmatika = angka1 * angka2;
+console.log(OperatorAritmatika);
+
+OperatorAritmatika = angka1 % angka2;
+console.log(OperatorAritmatika);
+
+//string
+let fname = "aldet";
+let lname = "kirana";
+let fullname = "nama saya:" + " " + fname + " " + lname;
+console.log(fullname);
+
+//alur pembacaan code dibaca dari kiri kekanan dan dari atas kebawah contoh
+
+let number = 10;
+console.log(number + 40); //hasilnya50
+
+number = 60; //number yang awalnya 10 diubah menjadi 60
+console.log(number + 40); //hasilnya 100
+
+number = false; // false = 0
+console.log(number + 10); //10
+
+number = true; //true = 1
+console.log(number + 10); //11
